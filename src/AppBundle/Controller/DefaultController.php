4ede41f9ff4e0120bfbf947549812328b8c43dee
Javier Eguiluz <javier.eguiluz@gmail.com>
2015-02-11 22:11:33 +0100

    Redirected the homepage to the admin index

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,10 +8,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/", name="homepage")
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        return $this->redirectToRoute('admin');
     }
 }
