33819bb66a15f93e3e7350edefee0c9f21392564
maxime.steinhausser <maxime.steinhausser@sensiolabs.com>
2015-05-19 17:50:28 +0200

    Simplify `app/config/routing.yml` and `AppBundle:AdminController` according to easyadminbundle#318

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -3,16 +3,9 @@
 namespace AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as EasyAdminController;
 
 class AdminController extends EasyAdminController
 {
-    /**
-     * @Route("/admin/", name="admin")
-     */
-    public function indexAction(Request $request)
-    {
-        return parent::indexAction($request);
-    }
+
 }
