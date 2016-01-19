109ed6c4fdadebc6b279f2c999b17f8e6e4ae995
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-01-19 22:31:23 +0100

    Added a Welcome page

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -3,7 +3,20 @@
 namespace AppBundle\Controller;
 
 use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as EasyAdminController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends EasyAdminController
 {
+    /**
+     * @Route("/", name="easyadmin")
+     */
+    public function indexAction(Request $request)
+    {
+        if (null === $request->query->get('entity')) {
+            return $this->render('demo/welcome.html.twig');
+        }
+
+        return parent::indexAction($request);
+    }
 }
