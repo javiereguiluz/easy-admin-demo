ffef47cb749d2847fef1c248ff8158b86c508e16
Javier Eguiluz <javier.eguiluz@gmail.com>
2017-10-26 10:15:50 +0200

    Fixed tests

--- tests/BackendTest.php
+++ tests/BackendTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle\Tests;
+namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\BrowserKit\Cookie;
@@ -12,7 +12,7 @@ class BackendTest extends WebTestCase
      */
     public function testBackendPagesLoadCorrectly($queryParameters)
     {
-        $client = $this->createAuthorizedClient();
+        $client = static::createClient();
         $client->request('GET', '/admin/?'.http_build_query($queryParameters));
 
         $this->assertTrue($client->getResponse()->isSuccessful());
@@ -53,34 +53,5 @@ class BackendTest extends WebTestCase
                 array('action' => 'edit', 'entity' => 'Product', 'id' => 1),
             ),
         );
-    }
-
-    /**
-     * Code copied from http://stackoverflow.com/a/27223293/2804294.
-     *
-     * @return \Symfony\Bundle\FrameworkBundle\Client
-     */
-    private function createAuthorizedClient()
-    {
-        $client = static::createClient();
-        $container = $client->getContainer();
-
-        $session = $container->get('session');
-        /** @var $userManager \FOS\UserBundle\Doctrine\UserManager */
-        $userManager = $container->get('fos_user.user_manager');
-        /** @var $loginManager \FOS\UserBundle\Security\LoginManager */
-        $loginManager = $container->get('fos_user.security.login_manager');
-        $firewallName = $container->getParameter('fos_user.firewall_name');
-
-        $user = $userManager->findUserBy(array('username' => 'john.smith'));
-        $loginManager->loginUser($firewallName, $user);
-
-        // save the login token into the session and put it in a cookie
-        $container->get('session')->set('_security_'.$firewallName,
-            serialize($container->get('security.token_storage')->getToken()));
-        $container->get('session')->save();
-        $client->getCookieJar()->set(new Cookie($session->getName(), $session->getId()));
-
-        return $client;
     }
 }
